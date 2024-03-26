1c724830744a8d829145ed2f039e522759d84404
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-03-26 14:06:52 +0600

    books data loaded to home page

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,22 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "./UtilsHome/Banner";
+import Books from "./UtilsHome/Books";
 
 const Home = () => {
+  const books = useLoaderData();
+  console.log(books)
+
   return (
     <div className="sm:px-1 md:px-3 lg:px-12">
       <Banner></Banner>
+      <div className="space-y-4 p-7">
+        <h3 className="text-center text-green-500 font-extrabold text-5xl py-3">Books</h3>
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+         {
+          books.map(book => <Books key={book.bookId} book={book}></Books>)
+         }
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Pages/UtilsHome/Books.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import { FaRegStar } from "react-icons/fa6";
+
+const Books = ({book}) => {
+  const {bookId, bookName, author, rating, category, tags, image} = book
+  console.log(typeof(bookId, bookName, author, rating, category, tags, image))
+  return (
+    <div>
+      <div className="card bg-base-100 p-4 pb-0 border-2 h-[460px]">
+        <figure><img src={image} className='h-40' alt="Book Image" /></figure>
+        <div className="card-body px-2 space-y-2">
+          <div className="flex gap-2 items-center justify-center"> Tags:
+            {
+              tags.map((tag, index) => <div className='bg-gray-200 p-2 rounded-md text-green-500' key={index}>#{tag}</div>)
+            }
+          </div>
+          <h2 className="text-center text-green-500 font-bold text-2xl">{bookName}</h2>
+          <p className='text-center text-gray-500 font-bold'>By: {author}</p>
+          <hr className='border-dashed border-t-2' />
+          <div className="flex justify-between font-bold">
+            <div>{category}</div>
+            <div className='flex items-center gap-1'>{rating}<FaRegStar /></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
+
+Books.propTypes = {
+  book : PropTypes.object,
+  bookId : PropTypes.string, 
+  bookName : PropTypes.string,
+  author : PropTypes.string, 
+  rating : PropTypes.string, 
+  category : PropTypes.string, 
+  tags : PropTypes.string, 
+  image : PropTypes.string,
+}
